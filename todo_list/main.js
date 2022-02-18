a28c5f4b7ff4b966c195cca8f313d7f2b38b1de2
Ksubin98 <subin.k1229@gmail.com>
2022-02-18 18:12:54 +0900

    failed to do list

--- todo_list/main.js
+++ todo_list/main.js
@@ -1,13 +1,49 @@
 'use strict';
 
-const listAdd = document.getElementById('listadd');
-const doAdd = document.getElementById('doadd');
-const list = document.getElementById('todolist');
-
-doAdd.addEventListener('click', clickButton);
+const input = document.querySelector('.listadd');
+const btn = document.querySelector('.doadd');
+const item = document.querySelector('.item');
 
 function clickButton() {
-    let add = document.createElement('todolist');
-    add.innerHTML = doAdd.value;
-    list.appendChild(add);
+    const inputText = input.value;
+    if(inputText === '') {
+        input.focus();
+        return ;
+    }
+
+    const newTodo = createList(inputText);
+    item.appendChild(newTodo);
+    input.value = "";
+    
 }
+
+function createList (inputText) {
+    const list = document.createElement('li');
+    list.setAttribute('class', 'ltem_list');
+
+    const todo = document.createElement('div');
+    todo.setAttribute('class', 'item');
+
+    const text = document.createElement('span');
+    text.setAttribute('class', 'name');
+    text.innerText = inputText;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'delete');
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can></i>';
+    deleteBtn.addEventListener('click', () => {
+        item.removeChild(list);
+    })
+
+
+    todo.appendChild(text);
+    todo.appendChild(deleteBtn);
+
+    list.appendChild(todo);
+
+    return list;
+}
+
+btn.addEventListener('click', () => {
+    clickButton();
+})
